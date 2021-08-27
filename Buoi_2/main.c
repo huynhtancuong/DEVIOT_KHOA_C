fbd62127b265b5875a81ad748d191c3cde927041
Huynh Tan Cuong <huynhtancuong.hcm@gmail.com>
2021-08-27 11:04:44 +0300

    Bai tap buoi 2 xong

--- Buoi_2/main.c
+++ Buoi_2/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define setbit(byte, pos) byte|=(1<<pos)
-#define clearbit(byte, pos) byte&=~(1<<pos)
-#define togglebit(byte, pos) byte^=(1<<pos)
+#define setbit(byte, pos)       byte|=(1<<pos)    //Bai tap 1
+#define clearbit(byte, pos)     byte&=~(1<<pos) //Bai tap 2
+#define togglebit(byte, pos)    byte^=(1<<pos) //Bai tap 3
 
-void printbit(int num)
+void printbit(int num)  //Bai tap 4
 {
     int bits=sizeof(num)*8;
     for (int i = 0; i<bits; i++)
@@ -13,10 +13,11 @@ void printbit(int num)
         printf("%d", ((num&(1<<bits-1))>>bits-1)&1);
         num<<=1;
     }
+    printf("\n");
 }
 //4 bytes
 //bits = sizeof(num)*8 = 4*8=32
-void BT5()
+void BT5()  //Bai tap 5
 {
     int num;
     printf("Nhap vao mot so: ");
@@ -25,10 +26,28 @@ void BT5()
     printbit(num);
 }
 
+void BT6()  //Bai tap 6
+{
+    int num;
+    printf("Nhap vao mot so: ");
+    scanf(" %d", &num);
+    int tmp=0;
+    while (num!=0)
+    {
+        tmp=tmp*10+(num%10);
+        num/=10;
+    }
+    while (tmp!=0)
+    {
+        printf("%d ", tmp%10);
+        tmp/=10;
+    }
+
+}
+
 int main()
 {
-    unsigned char a = 0b10100110;
     BT5();
-    //printf("\n%d", a);
+    BT6();
     return 0;
 }
